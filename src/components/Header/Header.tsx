2e6c97844833989f3ac61f7e0a223fa093988c16
김미르 <samkmr0220@gmail.com>
2025-08-26 17:03:12 +0900

    feat: Header 컴포넌트 완전 다국어 적용 - 한국어, 영어, 베트남어 번역 키 추가 - 로고 텍스트와 네비게이션 링크를 다국어 지원하도록 수정 - CommunityBanner 컴포넌트는 삭제된 상태 확인

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import '../../styles/Header.css';
 
 const Header: React.FC = () => {
+  const { t } = useTranslation();
+  
   return (
     <header className="header-container">
       <div className="header-logo">
-        <span>Fair </span>
-        <span>Work</span>
+        <span>{t('header.logo.fair')} </span>
+        <span>{t('header.logo.work')}</span>
       </div>
       <nav className="header-nav">
-        <a href="#about">ABOUT US</a>
-        <a href="#features">FEATURES</a>
-        <a href="#partners">PARTNERS</a>
+        <a href="#about">{t('header.navigation.aboutUs')}</a>
+        <a href="#features">{t('header.navigation.features')}</a>
+        <a href="#partners">{t('header.navigation.partners')}</a>
       </nav>
     </header>
   );
